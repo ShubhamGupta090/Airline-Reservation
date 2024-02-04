ace8dfbaed4e2f1459b173a56972e8c411aa2125
Shubham Gupta <131526044+ShubhamGupta090@users.noreply.github.com>
2024-02-04 15:32:37 +0530

    Create Date Program

---
+++ Date.h
@@ -0,0 +1,32 @@
+/*
+By-Shubham Gupta
+shubhamgupta0090903@gmial.com
+*/
+
+#include <iostream>
+
+class Date
+{
+	int day;
+	int month;
+	int year;
+public:
+	void make(int, int, int);
+	bool lessThan24(Date);
+	void showDate();	//prints out the entered date
+	
+};
+
+void Date::make(int d, int m, int y)  //this simple constructor assigns the parameter variables to the instanced variables
+{
+	day = d;
+	month = m;
+	year = y;
+}
+
+
+void Date::showDate()
+{
+	cout << day << "-" << month << "-" << year;		//prints out the date in a nice format
+
+}
